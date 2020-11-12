bfc033cce7520e314e4b55ff1461ad3a1d765c4c
Mine268 <mine268@foxmail.com>
2020-11-12 22:26:54 +0800

    t18

--- t18.bin_samecon/main.cpp
+++ t18.bin_samecon/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdlib>
+#include <queue>
 #include <cstring>
 #define N 30
 using namespace std;
@@ -13,12 +13,13 @@ typedef struct node
 typedef struct
 {
     char val;
-    int l;
-    int r;
+    int l, r;
 } dataNode, *dataSet;
 
 node *make(char);
-node *buildtree(dataSet, int);
+node *buildtree(dataSet, int, int);
+bool judgeSame(node*, node*);
+void printtree(node*);
 
 int main(void)
 {
@@ -26,48 +27,99 @@ int main(void)
     freopen("./test.out", "w", stdout);
 
     dataNode data1[N], data2[N];
-    int n;
-    node *t1, *t2;
+    int n = 0, m = 0, r1 = -1, r2 = -1;
+    node *t1 = nullptr, *t2 = nullptr;
 
     scanf("%d", &n);
+    getchar();
     for (int i = 0; i < n; i++)
     {
         char c1, c2, c3;
         scanf("%c %c %c", &c1, &c2, &c3);
+        getchar();
         data1[i].val = c1;
         data1[i].l = c2 != '-' ? c2 - '0' : -1;
         data1[i].r = c3 != '-' ? c3 - '0' : -1;
+        if (c1 == 'A')
+            r1 = i;
     }
-    for (int i = 0; i < n; i++)
+    scanf("%d", &m);
+    getchar();
+    for (int i = 0; i < m; i++)
     {
         char c1, c2, c3;
         scanf("%c %c %c", &c1, &c2, &c3);
+        getchar();
         data2[i].val = c1;
         data2[i].l = c2 != '-' ? c2 - '0' : -1;
         data2[i].r = c3 != '-' ? c3 - '0' : -1;
+        if (c1 == 'A')
+            r2 = i;
     }
 
-    t1 = buildtree(data1, n);
-    t2 = buildtree(data2, n);
+    t1 = buildtree(data1, r1, n);
+    t2 = buildtree(data2, r2, m);
+
+    if (judgeSame(t1, t2))
+        printf("Yes.\n");
+    else
+        printf("No.\n");
 
     return 0;
 }
 
-node *buildtree(dataSet ds, int n)
+node *buildtree(dataSet ds, int r, int n)
 {
-    int mark[N], root = 0;
-    memset(mark, 0, sizeof(int) * N);
-    for (int i = 0; i < n; i++)
+    if (r == -1 || r >= n)
+        return nullptr;
+    else
     {
-        if (ds[i].l != -1)
-            mark[ds[i].l] = 1;
-        if (ds[i].r != -1)
-            mark[ds[i].r] = 1;
+        node *sr = make(ds[r].val);
+        if (ds[r].l != r)
+            sr->left = buildtree(ds, ds[r].l, n);
+        else
+            return nullptr;
+        if (ds[r].r != r)
+            sr->right = buildtree(ds, ds[r].r, n);
+        else
+            return nullptr;
+        return sr;
     }
-    while (mark[root] == 1)
-        root++;
+}
 
-    return nullptr;
+void printtree(node *t)
+{
+    queue<node*> q;
+    q.push(t);
+    while (q.empty() == false)
+    {
+        node *tmp = q.front();
+        q.pop();
+        if (tmp == nullptr)
+            cout << '#';
+        else
+        {
+            cout << tmp->val;
+            q.push(tmp->left);
+            q.push(tmp->right);
+        }
+    }
+}
+
+bool judgeSame(node *r1, node *r2)
+{
+    if (r1 == nullptr && r2 == nullptr)
+        return true;
+    else if (r1 == nullptr || r2 == nullptr)
+        return false;
+    else
+    {
+        bool f1, f2, f3;
+        f1 = r1->val == r2->val;
+        f2 = judgeSame(r1->left, r2->left) && judgeSame(r1->right, r2->right);
+        f3 = judgeSame(r1->left, r2->right) && judgeSame(r1->right, r2->left);
+        return f1 && (f2 || f3);
+    }
 }
 
 node *make(char c)
